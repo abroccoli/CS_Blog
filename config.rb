47771771a3d9614c76da80e37d3409126764bafa
abroccoli <acbrocco@syr.edu>
2015-03-30 16:36:58 -0400

    initialized octopress blog for CS topics

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/CS_Blog/"
+http_images_path = "/CS_Blog/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/CS_Blog/fonts"
+css_dir = "public/CS_Blog/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
